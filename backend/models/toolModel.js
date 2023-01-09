cbb4b0134da340a5e1f62b1d7ad51e92bb5c63d6
NY1105 <yanwaiyin1105@gmail.com>
2023-01-09 08:38:52 +0800

    20230109-0839 (model init, tools database build 0.0)

--- backend/models/toolModel.js
+++ backend/models/toolModel.js
@@ -3,25 +3,44 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const toolSchema = new Schema(
-	// {
-	// 	title: {
-	// 		type: String,
-	// 		required: true,
-	// 	},
-	// 	reps: {
-	// 		type: Number,
-	// 		required: true,
-	// 	},
-	// 	load: {
-	// 		type: Number,
-	// 		required: true,
-	// 	},
-	// 	user_id: {
-	// 		type: String,
-	// 		required: true,
-	// 	},
-	// },
-	// { timestamps: true }
+	{
+		appName: {
+			type: String,
+			required: true,
+		},
+		appProviderName: {
+			type: String,
+			required: true,
+		},
+		appOfficialSiteURL: {
+			type: String,
+			required: true,
+		},
+		appTags: [String],
+		appDescription: {
+			type: String,
+		},
+		appRequirements: {
+			type: String,
+		},
+		nVisit: {
+			type: Number,
+		},
+		nSaved: {
+			type: Number,
+		},
+		createdBy: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
 )
+
+userSchema.path('appOfficialSiteURL').validate((val) => {
+	urlRegex =
+		/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/
+	return urlRegex.test(val)
+}, 'Invalid URL.')
 
 module.exports = mongoose.model('Tool', toolSchema)
