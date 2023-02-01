cdfcbfcd66c1f177ab3ea812a3a4024b3b4175c7
NY1105 <yanwaiyin1105@gmail.com>
2023-02-01 23:34:44 +0800

    temp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
+
+import Layout from './components/Layout'
 import Home from './pages/Home'
-import Navbar from './components/Navbar'
+// import Navbar from './components/Navbar'
 import Tags from './pages/Tags'
 import About from './pages/About'
 import Me from './pages/Me'
@@ -10,24 +12,26 @@ function App() {
 	return (
 		<div className="App">
 			<BrowserRouter>
-				<Navbar />
-				<div className="pages">
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/tags" element={<Tags />} />
-						<Route path="/about" element={<About />} />
-						<Route path="/me" element={<Me />} />
-						<Route
-							path="*"
-							element={
-								<>
-									<h1>Page Not Found</h1>
-									<Link to="/">Back to home</Link>
-								</>
-							}
-						/>
-					</Routes>
-				</div>
+				<Layout>
+					{/* <Navbar /> */}
+					<div className="pages">
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/tags" element={<Tags />} />
+							<Route path="/about" element={<About />} />
+							<Route path="/me" element={<Me />} />
+							<Route
+								path="*"
+								element={
+									<>
+										<h1>Page Not Found</h1>
+										<Link to="/">Back to home</Link>
+									</>
+								}
+							/>
+						</Routes>
+					</div>
+				</Layout>
 			</BrowserRouter>
 		</div>
 	)
